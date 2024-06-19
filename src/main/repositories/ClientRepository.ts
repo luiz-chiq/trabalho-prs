f46cab9bb65b8b304c6aa5b388728b0e2cddf40d
luiz-chiq <chiquetanoluiz@gmail.com>
2024-06-18 23:31:13 -0300

    feat: create ClientRepository

---
+++ src/main/repositories/ClientRepository.ts
@@ -0,0 +1,43 @@
+import { PrismaClient } from '@prisma/client'
+import { Client } from '../models/ClientModel'
+const prisma = new PrismaClient()
+
+export class ClientRepository {
+  async create(client: Client) {
+    return await prisma.client.create({
+      data: {
+        uuid: client.uuid,
+        name: client.name,
+        address: client.address,
+        phone: client.phone
+      }
+    })
+  }
+
+  async findAll() {
+    return await prisma.client.findMany()
+  }
+
+  async findById(uuid: string) {
+    return await prisma.client.findUnique({
+      where: { uuid }
+    })
+  }
+
+  async update(client: Client) {
+    return await prisma.client.update({
+      where: { uuid: client.uuid },
+      data: {
+        name: client.name,
+        address: client.address,
+        phone: client.phone
+      }
+    })
+  }
+
+  async delete(uuid: string) {
+    return await prisma.client.delete({
+      where: { uuid }
+    })
+  }
+}
