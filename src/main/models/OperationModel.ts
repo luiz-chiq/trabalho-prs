57cbe6b777e0699a0bde21922d27bf878ce7d873
luiz-chiq <chiquetanoluiz@gmail.com>
2024-06-18 23:31:26 -0300

    feat: create OperationModel

---
+++ src/main/models/OperationModel.ts
@@ -0,0 +1,42 @@
+import { v4 as uuidv4 } from 'uuid'
+import { Decimal } from 'decimal.js'
+import { OperationType } from './enums/OperationType'
+
+interface OperationDTO {
+  name: string
+  uuid: string
+  price: string // Convertido para string
+  type: OperationType
+}
+
+export class Operation {
+  name: string
+  uuid: string
+  price: Decimal
+  type: OperationType
+
+  constructor(name: string, price: Decimal, type: OperationType, uuid?: string) {
+    this.uuid = uuid || uuidv4()
+    this.name = name
+    this.price = price
+    this.type = type
+  }
+
+  toDTO(): OperationDTO {
+    return {
+      name: this.name,
+      uuid: this.uuid,
+      price: this.price.toString(), // Converte Decimal para string
+      type: this.type
+    }
+  }
+
+  static fromDTO(dto: OperationDTO): Operation {
+    return new Operation(
+      dto.name,
+      new Decimal(dto.price), // Converte string de volta para Decimal
+      dto.type,
+      dto.uuid
+    )
+  }
+}
