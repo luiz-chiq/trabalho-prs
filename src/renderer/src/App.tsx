da6a2f4c1997cfc67aed8f14e78bd91f7a46874c
luiz-chiq <chiquetanoluiz@gmail.com>
2024-06-24 20:01:50 -0300

    feat: add operations option to menu

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -2,6 +2,8 @@ import { Button, Flex, Typography } from 'antd'
 import { MemoryRouter as Router, Routes, Route, useNavigate } from 'react-router-dom'
 import { ClientForm } from './pages/Client/ClientForm'
 import { ClientList } from './pages/Client/ClientList'
+import { OperationForm } from './pages/Operation/OperationForm'
+import { OperationList } from './pages/Operation/OperationList'
 
 const Menu: React.FC = () => {
   const navigate = useNavigate()
@@ -12,6 +14,12 @@ const Menu: React.FC = () => {
         <Typography.Title level={3}>Menu</Typography.Title>
         <Button htmlType="button" onClick={() => navigate('/clients')}>
           Listar clientes
+        </Button>
+        <Button htmlType="button" onClick={() => navigate('/operations')}>
+          Listar Operações
+        </Button>
+        <Button htmlType="button" onClick={() => navigate('/financialRecords')}>
+          Listar Operações
         </Button>
       </Flex>
     </div>
@@ -25,6 +33,8 @@ export function App(): JSX.Element {
         <Route path="/" element={<Menu />} />
         <Route path="/createClient" element={<ClientForm />} />
         <Route path="/clients" element={<ClientList />} />
+        <Route path="/createOperation" element={<OperationForm />} />
+        <Route path="/operations" element={<OperationList />} />
       </Routes>
     </Router>
   )
