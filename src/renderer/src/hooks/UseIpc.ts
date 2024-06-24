d9028f01aa6696b61724af7871b8e010765f1dfb
luiz-chiq <chiquetanoluiz@gmail.com>
2024-06-24 19:37:50 -0300

    feat: create custom hook to use IPC communication

---
+++ src/renderer/src/hooks/UseIpc.ts
@@ -0,0 +1,21 @@
+import { useEffect, useCallback } from 'react'
+
+const { ipcRenderer } = window.electron
+
+const useIpc = (channel?: string, callback?: () => any) => {
+  if (channel && callback)
+    useEffect(() => {
+      ipcRenderer.on(channel, callback)
+      return () => {
+        ipcRenderer.removeListener(channel, callback)
+      }
+    }, [channel, callback])
+
+  const send = useCallback((channel: string, data?: any) => {
+    ipcRenderer.send(channel, data)
+  }, [])
+
+  return { send }
+}
+
+export default useIpc
