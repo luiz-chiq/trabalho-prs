07473c7e4b5822b912fca60f8c848e9209f411fc
luiz-chiq <chiquetanoluiz@gmail.com>
2024-06-24 20:00:55 -0300

    feat: add client option to menu

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,5 +1,7 @@
-import { Flex, Typography } from 'antd'
+import { Button, Flex, Typography } from 'antd'
 import { MemoryRouter as Router, Routes, Route, useNavigate } from 'react-router-dom'
+import { ClientForm } from './pages/Client/ClientForm'
+import { ClientList } from './pages/Client/ClientList'
 
 const Menu: React.FC = () => {
   const navigate = useNavigate()
@@ -8,6 +10,9 @@ const Menu: React.FC = () => {
     <div>
       <Flex gap="middle" vertical style={{ width: '200px' }} justify="center" align="center">
         <Typography.Title level={3}>Menu</Typography.Title>
+        <Button htmlType="button" onClick={() => navigate('/clients')}>
+          Listar clientes
+        </Button>
       </Flex>
     </div>
   )
@@ -18,6 +23,8 @@ export function App(): JSX.Element {
     <Router>
       <Routes>
         <Route path="/" element={<Menu />} />
+        <Route path="/createClient" element={<ClientForm />} />
+        <Route path="/clients" element={<ClientList />} />
       </Routes>
     </Router>
   )
