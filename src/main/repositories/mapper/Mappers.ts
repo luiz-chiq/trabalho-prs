e9ba6d2baab76ae8f3e65598bbfc122cb77ebc2b
luiz-chiq <chiquetanoluiz@gmail.com>
2024-06-24 01:23:36 -0300

    feat: create map to operation model

--- src/main/repositories/mapper/Mappers.ts
+++ src/main/repositories/mapper/Mappers.ts
@@ -1,5 +1,12 @@
+import Decimal from 'decimal.js'
 import { Client } from '../../models/ClientModel'
+import { Operation } from '../../models/OperationModel'
+import { OperationType } from '../../models/enums/OperationType'
 
 export const mapClient = (data: any): Client => {
   return new Client(data.name, data.address, data.phone, data.uuid)
+}
+
+export const mapOperation = (data: any): Operation => {
+  return new Operation(data.name, new Decimal(data.price), data.type as OperationType, data.uuid)
 }
