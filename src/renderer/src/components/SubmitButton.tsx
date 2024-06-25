f06a04ed130e25e60ba7fc9959ec5427f6c0faee
luiz-chiq <chiquetanoluiz@gmail.com>
2024-06-24 21:15:23 -0300

    feat: create generic submitButton

---
+++ src/renderer/src/components/SubmitButton.tsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import type { FormInstance } from 'antd'
+import { Button, Form } from 'antd'
+import useIpc from '@renderer/hooks/UseIpc'
+
+interface SubmitButtonProps {
+  form: FormInstance
+  ipcChannel: string
+}
+
+export const SubmitButton: React.FC<React.PropsWithChildren<SubmitButtonProps>> = ({
+  form,
+  ipcChannel,
+  children
+}) => {
+  const [submittable, setSubmittable] = React.useState<boolean>(false)
+
+  const { send } = useIpc()
+
+  const values = Form.useWatch([], form)
+
+  React.useEffect(() => {
+    form
+      .validateFields({ validateOnly: true })
+      .then(() => setSubmittable(true))
+      .catch(() => setSubmittable(false))
+
+    console.log(form.getFieldsValue())
+  }, [form, values])
+
+  return (
+    <Button
+      type="primary"
+      htmlType="button"
+      onClick={() => send(ipcChannel, form.getFieldsValue())}
+      disabled={!submittable}
+    >
+      {children}
+    </Button>
+  )
+}
