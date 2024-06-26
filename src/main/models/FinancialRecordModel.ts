398e1475a511c1dbd3fb715a77fef02b48a3ea96
luiz-chiq <chiquetanoluiz@gmail.com>
2024-06-26 00:05:51 -0300

    feat: delete financialRecord Dto

--- src/main/models/FinancialRecordModel.ts
+++ src/main/models/FinancialRecordModel.ts
@@ -3,31 +3,16 @@ import { v4 as uuidv4 } from 'uuid'
 import { Operation } from './OperationModel'
 import { OperationType } from './enums/OperationType'
 
-interface FinancialRecordDTO {
-  uuid: string
-  date: string
-  unitPrice: string
-  quantity: number
-  discount: number
-  totalPrice: string
-  operationType: OperationType
-  operationName: string
-  operationId: string
-  clientId: string
-  invoiceId: string
-}
-
 export class FinancialRecord {
   uuid: string
   date: Date
-  unitPrice: Decimal
+  unitPrice: number
   quantity: number
   discount: number
-  totalPrice: Decimal
+  totalPrice: number
   operationType: OperationType
   operationName: string
   operationId: string
-  clientId: string
   invoiceId: string
 
   constructor(
@@ -35,58 +20,24 @@ export class FinancialRecord {
     discount: number,
     operation: Operation,
     quantity: number,
-    clientId: string,
     invoiceId: string,
-    uuidid?: string,
-    totalPrice?: Decimal
+    uuid?: string,
+    totalPrice?: number
   ) {
-    this.uuid = uuidid || uuidv4()
+    this.uuid = uuid || uuidv4()
     this.date = date
     this.operationType = operation.type
     this.operationName = operation.name
     this.quantity = quantity
     this.discount = discount
-    this.unitPrice = operation.price
+    this.unitPrice = operation.price.toNumber()
     this.totalPrice =
       totalPrice ||
-      this.unitPrice.times(quantity).times(discount <= 1 && discount >= 0 ? 1 - discount : 1)
+      new Decimal(this.unitPrice)
+        .times(quantity)
+        .times(discount <= 1 && discount >= 0 ? 1 - discount : 1)
+        .toNumber()
     this.operationId = operation.uuid
-    this.clientId = clientId
     this.invoiceId = invoiceId
-  }
-
-  toDTO(): FinancialRecordDTO {
-    return {
-      uuid: this.uuid,
-      date: this.date.toISOString(),
-      unitPrice: this.unitPrice.toString(),
-      quantity: this.quantity,
-      discount: this.discount,
-      totalPrice: this.totalPrice.toString(),
-      operationType: this.operationType,
-      operationName: this.operationName,
-      operationId: this.operationId,
-      clientId: this.clientId,
-      invoiceId: this.invoiceId
-    }
-  }
-
-  static fromDTO(dto: FinancialRecordDTO): FinancialRecord {
-    const operation = new Operation(
-      dto.operationName,
-      new Decimal(dto.unitPrice),
-      dto.operationType,
-      dto.operationId
-    )
-    return new FinancialRecord(
-      new Date(dto.date),
-      dto.discount,
-      operation,
-      dto.quantity,
-      dto.clientId,
-      dto.invoiceId,
-      dto.uuid,
-      new Decimal(dto.totalPrice)
-    )
   }
 }
