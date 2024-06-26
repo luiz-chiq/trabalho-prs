e26cc80135c2675ab9ced4a69044b29327e6b88b
luiz-chiq <chiquetanoluiz@gmail.com>
2024-06-26 02:48:38 -0300

    feat: create financialRecordDto

--- src/main/models/FinancialRecordModel.ts
+++ src/main/models/FinancialRecordModel.ts
@@ -3,6 +3,19 @@ import { v4 as uuidv4 } from 'uuid'
 import { Operation } from './OperationModel'
 import { OperationType } from './enums/OperationType'
 
+interface FinancialRecordDto {
+  uuid: string
+  date: string
+  unitPrice: number
+  quantity: number
+  discount: number
+  totalPrice: number
+  operationType: OperationType
+  operationName: string
+  operationId: string
+  invoiceId: string
+}
+
 export class FinancialRecord {
   uuid: string
   date: Date
@@ -38,6 +51,21 @@ export class FinancialRecord {
         .times(discount <= 1 && discount >= 0 ? 1 - discount : 1)
         .toNumber()
     this.operationId = operation.uuid
-    this.invoiceId = invoiceId
+    this.invoiceId = invoiceId || '-'
+  }
+
+  toDTO(): FinancialRecordDto {
+    return {
+      uuid: this.uuid,
+      date: this.date.toLocaleDateString(),
+      unitPrice: this.unitPrice,
+      quantity: this.quantity,
+      discount: this.discount,
+      totalPrice: this.totalPrice,
+      operationType: this.operationType,
+      operationName: this.operationName,
+      operationId: this.operationId,
+      invoiceId: this.invoiceId
+    }
   }
 }
